1f5cc05eafe29517c50649e8eda91078c6a92dae
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-09-26 12:13:28 -0700

    Spring Initializr backbone.

---
+++ src/main/java/com/cs157a1/payMe/PayMeApplication.java
@@ -0,0 +1,12 @@
+package com.cs157a1.payMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMeApplication.class, args);
+	}
+}
