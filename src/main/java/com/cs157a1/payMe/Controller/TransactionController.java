ef5d4b7d93b250514d0d110d3da73e6127dbe0bd
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-11-27 22:57:23 -0800

    Back-end needed for transfer page

--- src/main/java/com/cs157a1/payMe/Controller/TransactionController.java
+++ src/main/java/com/cs157a1/payMe/Controller/TransactionController.java
@@ -4,9 +4,14 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,14 +34,17 @@ public class TransactionController {
 	}
 	
 	
-	@ModelAttribute("transferList")
+	@ModelAttribute("transferAccount")
 	public TransactionList getList() {
 		TransactionList list = new TransactionList();
 		return list;
 	}
 	
 	public class TransactionList{
+		@NotNull
+		@NotBlank
 		public String transactionList;
+		
 		public int amount;
 		public TransactionList() {
 			
@@ -47,7 +55,7 @@ public class TransactionController {
 			this.amount = amount;
 		}
 		
-		public String gettransactionList() {
+		public String getTransactionList() {
 			return transactionList;
 		}
 		
@@ -65,14 +73,20 @@ public class TransactionController {
 	}
 	
 	@RequestMapping(value="/transfer", method = RequestMethod.GET)
-	public String getTransferForm(@ModelAttribute("transferList")TransactionList str,ModelMap map) {
+	public String getTransferForm(@ModelAttribute("transferAccount")TransactionList str,ModelMap map) {
 		map.addAttribute("transferAccount", str);
 		return "transfer";
 	}
 
 	@RequestMapping(value="/transfer", method = RequestMethod.POST)
-	public String sendTransferForm(@ModelAttribute("transferAccount") Account[] transferAccountList, ModelMap map) {
+	public String sendTransferForm(@Valid @ModelAttribute("transferAccount") TransactionList str, BindingResult binding) {
+		String[] usernames = str.getTransactionList().split(",");
+		if(binding.hasErrors()) {
+			return "transfer";
+		}
+		else {
 		return "transfer";
+		}
 	}
 	
 	
